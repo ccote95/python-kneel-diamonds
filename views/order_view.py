1daf82127d4c3c4198e3cd9d77de8618af3e027f
Chad Cote <chadcote58@gmail.com>
2024-07-19 14:39:15 -0400

    Wrote logic to get a single order

--- views/order_view.py
+++ views/order_view.py
@@ -25,3 +25,26 @@ def list_orders():
         serialized_orders = json.dumps(orders)
 
     return serialized_orders
+
+
+
+def single_order(pk):
+    with sqlite3.connect('./kneeldiamonds.sqlite3') as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute(
+            """
+            SELECT
+                o.id,
+                o.metal_id,
+                o.size_id,
+                o.style_id
+            FROM Orders o
+            WHERE o.id = ?
+
+            """,(pk,))
+        query_results = db_cursor.fetchone()
+
+        serialized_order = json.dumps(dict(query_results))
+    return serialized_order
